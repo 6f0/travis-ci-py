0d0df36d9191304c3b6193c7864cb15e5dae22d5
Blake <bmcculley@users.noreply.github.com>
2016-02-22 14:01:49 -0500

    example unit test

---
+++ example.py
@@ -0,0 +1,18 @@
+import unittest
+import example2Code
+
+class TestStringMethods(unittest.TestCase):
+	
+	def test_upper(self):
+		self.assertEqual(example2Code.toUpper('foo'), 'FOO')
+		
+	def test_isupper(self):
+		self.assertTrue(example2Code.testUpper('FOO'))
+		self.assertFalse(example2Code.testUpper('Foo'))
+	
+	def test_split(self):
+		s = 'hello world'
+		self.assertEqual(example2Code.stringSplit(s), ['hello', 'world'])
+		
+if __name__ == '__main__':
+    unittest.main()
