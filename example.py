3a7458d7db0e76e9291dba25cf5f192dc341d541
Blake <bmcculley@users.noreply.github.com>
2016-02-22 14:39:29 -0500

    import from cwd

--- example.py
+++ example.py
@@ -1,20 +1,22 @@
 import unittest
+import os
+import sys
+sys.path.append(os.getcwd())
+
+import example2Code
 
 class TestStringMethods(unittest.TestCase):
-
-  def test_upper(self):
-      self.assertEqual('foo'.upper(), 'FOO')
-
-  def test_isupper(self):
-      self.assertTrue('FOO'.isupper())
-      self.assertFalse('Foo'.isupper())
-
-  def test_split(self):
-      s = 'hello world'
-      self.assertEqual(s.split(), ['hello', 'world'])
-      # check that s.split fails when the separator is not a string
-      with self.assertRaises(TypeError):
-          s.split(2)
-
+	
+	def test_upper(self):
+		self.assertEqual(example2Code.toUpper('foo'), 'FOO')
+		
+	def test_isupper(self):
+		self.assertTrue(example2Code.testUpper('FOO'))
+		self.assertFalse(example2Code.testUpper('Foo'))
+	
+	def test_split(self):
+		s = 'hello world'
+		self.assertEqual(example2Code.stringSplit(s), ['hello', 'world'])
+		
 if __name__ == '__main__':
     unittest.main()
